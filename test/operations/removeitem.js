400f8dfbc73ef25a2b59689c19ef2708260e2cc9
Franco Melandri <franco.melandri@gmail.com>
2014-12-11 11:44:40 +0100

    Added remove item

---
+++ test/operations/removeitem.js
@@ -0,0 +1,21 @@
+
+var RemoveItemOperation = function() {};
+
+RemoveItemOperation.prototype.POST = function(request, operation, params, callback) {
+	
+	var env = operation.env;
+	var options = {
+		json : true,
+		body: params,
+		method: 'POST',
+		url : env.https + operation.session.settings.siteCode + operation.url,
+	};
+
+    operation.execute(request, options, callback, function(body){ return body;} );
+};
+
+RemoveItemOperation.prototype.Url = function() {
+	return '/cart/removeitem';
+}
+
+module.exports = new RemoveItemOperation();
