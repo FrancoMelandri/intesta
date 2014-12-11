21e7fbd305014ed864b3b44db65492093a9a948b
Franco Melandri <franco.melandri@gmail.com>
2014-12-11 16:57:51 +0100

    Added validate login

--- test/operations/login.js
+++ test/operations/login.js
@@ -14,6 +14,18 @@ LoginOperation.prototype.POST = function(request, operation, params, callback) {
     operation.execute(request, options, callback, function(body){ return body;} );
 };
 
+LoginOperation.prototype.GET = function(request, operation, params, callback) {
+	
+	var env = operation.env;
+	var options = {
+		qs: params,
+		method: 'GET',
+		url : env.https + operation.session.settings.siteCode + operation.url,
+	};
+
+    operation.execute(request, options, callback, function(body){ return body;} );
+};
+
 LoginOperation.prototype.Url = function() {
 	return '/myoox/login';
 }
