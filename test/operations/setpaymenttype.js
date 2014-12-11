b0d97201d59eee235430ca3c83811f5f6c2969a6
Franco Melandri <franco.melandri@gmail.com>
2014-12-11 11:48:24 +0100

    Added set payment

---
+++ test/operations/setpaymenttype.js
@@ -0,0 +1,21 @@
+
+var SetPaymentTypeOperation = function() {};
+
+SetPaymentTypeOperation.prototype.POST = function(request, operation, params, callback) {
+	
+	var env = operation.env;
+	var options = {
+		json : true,
+		body: params,
+		method: 'POST',
+		url : env.https + operation.session.settings.siteCode + operation.url,
+	};
+
+    operation.execute(request, options, callback, function(body){ return body;} );
+};
+
+SetPaymentTypeOperation.prototype.Url = function() {
+	return '/cart/setpaymenttype';
+}
+
+module.exports = new SetPaymentTypeOperation();
