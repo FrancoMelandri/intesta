322042b86d75ca672f486ca392238a5195a819e9
Franco Melandri <franco.melandri@gmail.com>
2014-12-12 18:39:53 +0100

    added menu

---
+++ test/operations/menu.js
@@ -0,0 +1,21 @@
+
+var MenuOperation = function() {};
+
+MenuOperation.prototype.GET = function(request, operation, params, callback) {	
+	var env = operation.env;
+	var options = {
+		qs : params,
+		url : env.http + operation.session.settings.siteCode + operation.url,
+	};
+    operation.execute(request, options, callback, function(body) { 
+    	var result = JSON.parse(body);
+		console.log(result);
+		return result;
+		} );
+};
+
+MenuOperation.prototype.Url = function() {
+	return '/menu';
+}
+
+module.exports = new MenuOperation();
