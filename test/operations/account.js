ba3f598a37cbe7fbdd0882e805f56e16c4211cd9
Franco Melandri <franco.melandri@gmail.com>
2014-12-17 12:40:37 +0100

    Added address book resources

--- test/operations/account.js
+++ test/operations/account.js
@@ -27,7 +27,22 @@ AccountOperation.prototype.PUT = function(request, operation, params, callback) 
         method : "PUT",
         url : env.https + operation.session.settings.siteCode + operation.url,
     };
-    operation.execute(request, options, callback, function(body){ body;} );
+    operation.execute(request, options, callback, function(body){ return JSON.parse(body);} );
+};
+
+AccountOperation.prototype.POST = function(request, operation, params, callback) {
+    
+    var env = operation.env;
+    var options = {
+        headers: {
+                'User-Agent': operation.session.settings.userAgent
+        },      
+        json : true,
+        body: params,
+        method : "POST",
+        url : env.https + operation.session.settings.siteCode + operation.url,
+    };
+    operation.execute(request, options, callback, function(body){ return JSON.parse(body);} );
 };
 
 
