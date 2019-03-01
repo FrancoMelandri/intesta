d3c88b6e3afa209563d3ffa3ba45457b87d31aa7
Franco Melandri <franco.melandri@gmail.com>
2019-03-01 12:11:19 +0100

    cleaning code

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function(launchData) {
-  chrome.app.window.create('app.html', {
-    width: 1024,
-    height: 720,
-    minWidth: 1024,
-    minHeight: 720
-  });
-});
--- test/intesta.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-IF EXIST "C:\\Program Files\\nodejs\\node.exe" (
-	echo "C:\\Program Files\\nodejs\\node.exe" test.js sessions
-	"C:\\Program Files\\nodejs\\node.exe" test.js sessions
-) ELSE (
-	"C:\\Program Files (x86)\\nodejs\\node.exe" test.js sessions
-)
