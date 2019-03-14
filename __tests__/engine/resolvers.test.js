1cb5710c50378438596ef9f3579d2b4c432028c4
FrancoMelandri <franco.melandri@gmail.com>
2019-03-14 14:21:14 +0100

    removing duplicated code

--- __tests__/engine/resolvers.test.js
+++ __tests__/engine/resolvers.test.js
@@ -90,4 +90,49 @@ describe('Testing resolvers', () => {
         }
         expect(options).toMatchObject(expected)
     });
+
+    test('Should create options for PUT verb', () => {
+
+        const operation = {
+            name: 'operations',
+            operation: 'api',
+            params: {
+                param1: '{{{Test.param}}}',
+                param2: 'value2'
+            },
+            headers: {
+                header1: '{{{Test.header}}}',
+                header2: 'value2'
+            }
+        }
+        const session = {
+            settings: {
+                url: 'https://example.com'
+            }
+        }
+        const api = {
+            name: 'api',
+            path: '/api',
+            verb: 'POST',
+            params: ['param1', 'param2'],
+            headers: ['header1', 'header2']
+        }
+        context.add('Test', {header: 'HEADER',
+            param: 'PARAM'})
+        const options = sut['POST'].options(context, session, api, operation)
+        const expected = {
+            headers: {
+                header1: 'HEADER',
+                header2: 'value2'
+            },
+            body: {
+                param1: 'PARAM',
+                param2: 'value2'
+            },
+            method: 'POST',
+            url: 'https://example.com/api'
+        }
+        expect(options).toMatchObject(expected)
+    });
+
 });
--- engine/resolvers.js
+++ engine/resolvers.js
@@ -6,27 +6,39 @@ const fill = (context, descriptors, values) => {
     return headers
 }
 
+const options = (context, session, api, operation)  => {
+    return {
+        headers: fill(context, api.headers, operation.headers),
+        method: api.verb,
+        json: true,
+        qs: fill(context, api.params, operation.params),
+        body: fill(context, api.params, operation.params),
+        url: session.settings.url + api.path,
+    }
+}
+
 const resolvers = {
     GET: {
         options: (context, session, api, operation) => {
-            return {
-                headers: fill(context, api.headers, operation.headers),
-                qs: fill(context, api.params, operation.params),
-                method : api.verb,
-                url : session.settings.url + api.path,
-            }
+            const result = options(context, session, api, operation)
+            result.qs = fill(context, api.params, operation.params)
+            return result
         },
         getBody: (body) => JSON.parse(body)
     },
     POST: {
         options: (context, session, api, operation) => {
-            return {
-                headers: fill(context, api.headers, operation.headers),
-                json: true,
-                body: fill(context, api.params, operation.params),
-                method : api.verb,
-                url : session.settings.url + api.path,
-            }
+            const result = options(context, session, api, operation)
+            result.body = fill(context, api.params, operation.params)
+            return result
+        },
+        getBody: (body) => body
+    },
+    PUT: {
+        options: (context, session, api, operation) => {
+            const result = options(context, session, api, operation)
+            result.body = fill(context, api.params, operation.params)
+            return result
         },
         getBody: (body) => body
     }
