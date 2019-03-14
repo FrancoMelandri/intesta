c3a330aeeafdceb1b6e86f1c2718f66c1e3f8905
FrancoMelandri <franco.melandri@gmail.com>
2019-03-14 15:32:28 +0100

    refactoring oprions builder

--- engine/resolvers.js
+++ engine/resolvers.js
@@ -1,45 +1,56 @@
 
-const fill = (context, descriptors, values) => {
-    const headers = {}
-    descriptors
-        .forEach (_ => headers[_] = context.getValue(values[_]))
-    return headers
-}
+const optionsBuilder = (context, session, api, operation) => {
+    const _context = context
+    const _session = session
+    const _api = api
+    const _operation = operation
+    const _result = {}
 
-const options = (context, session, api, operation)  => {
+    const fill = (context, descriptors, values) => {
+        const params = {}
+        descriptors
+            .forEach (_ => params[_] = context.getValue(values[_]))
+        return params
+    }
+    
     return {
-        headers: fill(context, api.headers, operation.headers),
-        method: api.verb,
-        json: true,
-        qs: fill(context, api.params, operation.params),
-        body: fill(context, api.params, operation.params),
-        url: session.settings.url + api.path,
+        qs: function () {
+            _result.qs = fill(_context, _api.params, _operation.params)
+            return this
+        },
+
+        body: function () {
+            _result.json = true
+            _result.body = fill(_context, _api.params, _operation.params)
+            return this
+        },
+
+        build: function () {
+            _result.headers = fill(_context, _api.headers, _operation.headers)
+            _result.method = _api.verb
+            _result.url = _session.settings.url + _api.path
+            return _result
+        }
     }
 }
 
 const resolvers = {
     GET: {
-        options: (context, session, api, operation) => {
-            const result = options(context, session, api, operation)
-            result.qs = fill(context, api.params, operation.params)
-            return result
-        },
+        options: (context, session, api, operation) => optionsBuilder(context, session, api, operation)
+                                                            .qs()
+                                                            .build(),
         getBody: (body) => JSON.parse(body)
     },
     POST: {
-        options: (context, session, api, operation) => {
-            const result = options(context, session, api, operation)
-            result.body = fill(context, api.params, operation.params)
-            return result
-        },
+        options: (context, session, api, operation) => optionsBuilder(context, session, api, operation)
+                                                            .body()
+                                                            .build(),
         getBody: (body) => body
     },
     PUT: {
-        options: (context, session, api, operation) => {
-            const result = options(context, session, api, operation)
-            result.body = fill(context, api.params, operation.params)
-            return result
-        },
+        options: (context, session, api, operation) => optionsBuilder(context, session, api, operation)
+                                                            .body()
+                                                            .build(),
         getBody: (body) => body
     }
 }
